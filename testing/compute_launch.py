89e965752ff2ccb79774180840520c125cb2c681
Dmytro Kysylychyn <dkysylychyn@1deb6bca-397d-4862-8243-b4303f4a23b3.fritz.box>
2025-03-15 15:09:43 +0100

    added testing folder

---
+++ testing/compute_launch.py
@@ -0,0 +1,21 @@
+"""
+Before you begin, make sure to run this command in your terminal to install pytest:
+* pip install -U pytest
+
+Then, to run pytest, just enter:
+* pytest
+
+Right now, not all of the tests should pass. 
+Fix the function to pass all its tests! 
+Once all your tests pass, try writing some additional unit tests of your own!
+"""
+def days_until_launch(current_day, launch_day):
+    """"Returns the days left before launch.
+    
+    current_day (int) - current day in integer
+    launch_day (int) - launch day in integer
+    """
+    return launch_day - current_day
+
+if __name__ == "__main__":
+    print(days_until_launch(22,26))
